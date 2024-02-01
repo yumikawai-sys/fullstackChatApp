b9130e353b02aed05facc97c93dea6d0835c79b8
Yumi_Kawai <yycharlottetown@gmail.com>
2024-01-31 23:39:50 -0400

    2nd commit

--- login.php
+++ login.php
@@ -11,8 +11,25 @@
         <section class="form login">
             <header>Realtime Chat App</header>
             <form action="#" method="POST" enctype="ultipart/form-data" autocomplete="off">
-                
+                <div class="error-text"></div>
+                <div class="field input">
+                    <label>Email Address</label>
+                    <input type="text" name="email" placeholder="Enter your email" required>
+                </div>
+                <div class="field input">
+                    <label>Password</label>
+                    <input type="password" name="password"placeholder="Enter your password"required>
+                    <i class="fas fa-eye"></i>
+                </div>
+                <div class="field button">
+                    <input type="submit" name="submit" value="Continue to chat">
+                </div>
             </form>
+            <div class="link">Not yet signed up? <a href="index.php">Signup Now</a></div>
         </section>
     </div>
+
+    <script src="javascript/pass-show-hide.js"></script>
+    <script src="javascript/login.js"></script>
 </body>
+</html>
